5bc1774988a67c07986367b1a4dee45eca7e99e0
Jacob <cptjakey@gmail.com>
2023-10-16 20:59:07 +1000

    Search functionality WIP

--- a3_group53/EventsAround/views.py
+++ a3_group53/EventsAround/views.py
@@ -10,3 +10,13 @@ def index():
     events = Event.query.all()    
     return render_template('index.html', events=events)
     #TODO: add filtering
+
+@mainbp.route('/search')
+def search():
+    if request.args['search']:
+        print(request.args['search'])
+        evnt = "%" + request.args['search'] + '%'
+        events = Event.query.filter(Event.description.like(evnt)).all()
+        return render_template('index.html', events=events)
+    else:
+        return redirect(url_for('main.index'))
