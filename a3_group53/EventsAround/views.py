6106ea38ef36eb19ff4d0c23de7c4ca1e4cfec8c
Ruslan Draganov <ruslandraganov999@gmail.com>
2023-10-16 16:25:07 +1000

    Moved /search route to events.py

--- a3_group53/EventsAround/views.py
+++ a3_group53/EventsAround/views.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, request, redirect,url_for
 from .models import Event
+
+
 mainbp = Blueprint('main', __name__)
 
 
@@ -7,15 +9,4 @@ mainbp = Blueprint('main', __name__)
 def index():
     events = Event.query.all()    
     return render_template('index.html', events=events)
-    #TODO: add filtering
-
-@mainbp.route('/search')
-def search():
-    if request.args['search']:
-        print(request.args['search'])
-        evnt = "%" + request.args['search'] + '%'
-        event = Event.query.filter(Event.description.like(evnt)).all()
-        return render_template('index.html', event=event)
-    else:
-        return redirect(url_for('main.index'))
     #TODO: add filtering
