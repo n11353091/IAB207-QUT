afa82a7f28cf7b76ee7627dc63acac3e70fc2596
Ruslan Draganov <ruslandraganov999@gmail.com>
2023-10-16 15:53:25 +1000

    __init__.py: Error handling routes + blueprints html's: error handling templates

--- a3_group53/EventsAround/__init__.py
+++ a3_group53/EventsAround/__init__.py
@@ -1,10 +1,12 @@
 #from package import Class
-from flask import Flask 
+from flask import Flask, render_template 
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+
 db=SQLAlchemy()
+app=Flask(__name__)
 
 #create a function that creates a web application
 # a web server will run this web application
@@ -29,10 +31,10 @@ def create_app():
     login_manager.init_app(app)
 
     #create a user loader function takes userid and returns User
-    #from .models import User  # importing here to avoid circular references
-    #@login_manager.user_loader
-    #def load_user(user_id):
-    #    return User.query.get(int(user_id))
+    from .models import User  # importing here to avoid circular references
+    @login_manager.user_loader
+    def load_user(user_id):
+       return User.query.get(int(user_id))
 
     #importing views module here to avoid circular references
     # a common practice.
@@ -42,7 +44,41 @@ def create_app():
     from . import auth
     app.register_blueprint(auth.bp)
     
+    from . import events
+    app.register_blueprint(events.bp)
+
+    from . import orders
+    app.register_blueprint(orders.bp)
+
     return app
 
+@app.errorhandler(400) 
+# inbuilt function which takes error as parameter 
+def not_found(e): 
+  return render_template("errors/400.html"), 400
 
+@app.errorhandler(401)
+# inbuilt function which takes error as parameter
+def unauthorized(e):
+    return render_template("errors/401.html"), 401
 
+@app.errorhandler(403)
+def forbidden(e):
+    return render_template("errors/403.html"), 403
+
+@app.errorhandler(404) 
+# inbuilt function which takes error as parameter 
+def not_found(e): 
+  return render_template("errors/404.html"), 404
+
+@app.errorhandler(405)
+def method_not_allowed(e):
+    return render_template("errors/405.html"), 405
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("errors/500.html"), 500
+
+@app.errorhandler(503)
+def service_unavailable(e):
+    return render_template("errors/503.html"), 503
