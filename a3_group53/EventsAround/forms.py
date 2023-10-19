1340e6b393f6961fa2e618285fb6f4f2cb43e6a1
MursalMir <146822678+MursalMir@users.noreply.github.com>
2023-10-19 21:09:04 +1000

    Update forms.py
    removed the cancel button as that was incorrect

--- a3_group53/EventsAround/forms.py
+++ a3_group53/EventsAround/forms.py
@@ -36,7 +36,6 @@ class EventForm(FlaskForm):
     FileAllowed(ALLOWED_FILE, message='Only supports png,jpg,JPG,PNG')])
 
   submit = SubmitField("Create")
-  cancel = SubmitField('Cancel Event')
 
 #creates the login information
 class LoginForm(FlaskForm):
