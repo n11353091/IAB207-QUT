129f5e631a992fd29c89561011450246976dc5b6
Mursal Mir <n11284161@qut.edu.au>
2023-10-24 17:24:50 +1000

    Created and managed html, where users can edit or delete their specific events

--- a3_group53/EventsAround/forms.py
+++ a3_group53/EventsAround/forms.py
@@ -8,9 +8,9 @@ ALLOWED_FILE = {'PNG','JPG','png','jpg'}
 
 #Create new event
 class EventForm(FlaskForm):
-  status = SelectField('Status', choices=[('open', 'Open'), ('closed', 'Closed'), ('sold out', 'Sold out')], default='Open')
-  name = StringField('What is the name of the event?', validators=[InputRequired()])
-  type = SelectField(
+    status = SelectField('Status', choices=[('open', 'Open'), ('closed', 'Closed'), ('sold out', 'Sold out')], default='Open')
+    name = StringField('What is the name of the event?', validators=[InputRequired()])
+    type = SelectField(
         'What is your employment type?',
         coerce=str,
         default='',
@@ -25,17 +25,18 @@ class EventForm(FlaskForm):
             ('Tradeshows', 'Tradeshows'),
         )
     )
-  event_date = DateField('What is the date of the event?', validators=[InputRequired()])
-  start_time = TimeField('What is the start time of the event?', validators=[InputRequired()])
-  end_time = TimeField('What is the finish time of the event?', validators=[InputRequired()])
-  location = StringField('What is the location of your event?', validators=[InputRequired()])
-  description = TextAreaField("Add a description of what value your event will provide for it's attendees:", validators=[InputRequired()])
-  expire_date = DateField('When would you like your event to expire?', validators=[InputRequired()])
-  image = FileField('Add a picture of what your event will look like:', validators=[
-    FileRequired(message='Image cannot be empty'),
-    FileAllowed(ALLOWED_FILE, message='Only supports png,jpg,JPG,PNG')])
+    event_date = DateField('What is the date of the event?', validators=[InputRequired()])
+    start_time = TimeField('What is the start time of the event?', validators=[InputRequired()])
+    end_time = TimeField('What is the finish time of the event?', validators=[InputRequired()])
+    location = StringField('What is the location of your event?', validators=[InputRequired()])
+    description = TextAreaField("Add a description of what value your event will provide for it's attendees:", validators=[InputRequired()])
+    expire_date = DateField('When would you like your event to expire?', validators=[InputRequired()])
+    image = FileField('Add a picture of what your event will look like:', validators=[
+        FileRequired(message='Image cannot be empty'),
+        FileAllowed(ALLOWED_FILE, message='Only supports png,jpg,JPG,PNG')])
 
-  submit = SubmitField("Create")
+    submit = SubmitField("Create")
+
 
 #creates the login information
 class LoginForm(FlaskForm):
